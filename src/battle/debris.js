8db5c1c096dff8b1829c5307f9a4441e0131ddc0
Becky <becky@monometric.net>
2016-09-01 23:05:07 +0200

    randomize debrit orientation

--- src/battle/debris.js
+++ src/battle/debris.js
@@ -1,29 +1,32 @@
-export default function Debris(x, y) {
-    this.position = {x: x, y: y};
-    this.alive = true;
+export default class Debris {
+    constructor(x, y) {
+        this.position = {x: x, y: y};
+        this.orientation = Math.random() < .5 ? 0.5 : - 0.5;
+        this.alive = true;
+    }
+
+    update() {
+
+    };
+
+    draw(screen) {
+        screen.addCall(
+            this.position.x, this.position.y,
+            this.position.x + this.position.y - this.orientation,
+            () => {
+                screen.ctx.fillStyle = '#b86';
+                screen.ctx.transform(1, this.orientation, 0, 1, 0, 0);
+                screen.ctx.fillRect(-10, -10, 10, 20);
+            }
+        );
+        screen.addCall(
+            this.position.x, this.position.y,
+            this.position.x + this.position.y + this.orientation,
+            () => {
+                screen.ctx.fillStyle = '#b86';
+                screen.ctx.transform(1, this.orientation, 0, 1, 0, 0);
+                screen.ctx.fillRect(0, -10, 10, 20);
+            }
+        );
+    }
 }
-
-Debris.prototype.update = function () {
-
-};
-
-Debris.prototype.draw = function (screen) {
-    screen.addCall(
-        this.position.x, this.position.y,
-        this.position.x + this.position.y - .5,
-        function () {
-            screen.ctx.fillStyle = '#b86';
-            screen.ctx.transform(1, 0.5, 0, 1, 0, 0);
-            screen.ctx.fillRect(-10, -10, 10, 20);
-        }
-    );
-    screen.addCall(
-        this.position.x, this.position.y,
-        this.position.x + this.position.y + .5,
-        function () {
-            screen.ctx.fillStyle = '#b86';
-            screen.ctx.transform(1, 0.5, 0, 1, 0, 0);
-            screen.ctx.fillRect(0, -10, 10, 20);
-        }
-    );
-};
